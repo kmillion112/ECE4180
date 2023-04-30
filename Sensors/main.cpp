540f1a46adeeecda0144e3fdaa3eff3a54bd71f6
kmillion112 <131431920+kmillion112@users.noreply.github.com>
2023-04-30 00:06:37 -0400

    Add files via upload

--- Sensors/main.cpp
+++ Sensors/main.cpp
@@ -2,12 +2,15 @@
 #include "mbed.h"
 #include "HTU21D.h"
 #include "MPL3115A2.h"
+#include "LSM9DS1.h"
 
 #define MPL3115A2_I2C_ADDRESS (0x60);
 
 Serial pc(USBTX, USBRX);
 
 HTU21D temphumid(p9, p10);
+
+LSM9DS1 IMU(p9, p10, 0xD6, 0x3C);
 
 AnalogIn lightSensor(p20); 
 
@@ -18,6 +21,36 @@ int sample_humid;
 
 float lightLevel = 0;
 
+#define PI 3.14159
+
+#define DECLINATION -4.94 // Declination (degrees) in Atlanta,GA.
+
+void printAttitude(float ax, float ay, float az, float mx, float my, float mz)
+{
+    float roll = atan2(ay, az);
+    float pitch = atan2(-ax, sqrt(ay * ay + az * az));
+
+    mx = -mx;
+    float heading;
+    
+    if (my == 0.0)
+        heading = (mx < 0.0) ? 180.0 : 0.0;
+    else
+        heading = atan2(mx, my)*360.0/(2.0*PI);
+    
+    heading -= DECLINATION;
+    
+    if(heading>180.0) heading = heading - 360.0;
+    else if(heading<-180.0) heading = 360.0 + heading;
+    else if(heading<0.0) heading = 360.0  + heading;
+
+    pitch *= 180.0 / PI;
+    roll  *= 180.0 / PI;
+
+    pc.printf("Pitch: %f,    Roll: %f degress\n\r",pitch,roll);
+    pc.printf("Magnetic Heading: %f degress\n\r",heading);
+}
+
 int main()
 {
     double alt, bar, temp ;
@@ -26,6 +59,11 @@ int main()
     
     pc.printf("Beginning...");
     pc.printf("\n\r");
+
+    IMU.begin();
+    if (!IMU.begin()) {
+        pc.printf("Failed to communicate with LSM9DS1.\n");
+    }
 
     wait(1);
 
@@ -42,6 +80,15 @@ int main()
 
         lightLevel = lightSensor;
 
+        while(!IMU.tempAvailable());
+        IMU.readTemp();
+        while(!IMU.magAvailable(X_AXIS));
+        IMU.readMag();
+        while(!IMU.accelAvailable());
+        IMU.readAccel();
+        while(!IMU.gyroAvailable());
+        IMU.readGyro();
+
         pc.printf("Temperature: %d F\n\r", sample_ftemp);
         pc.printf("Temperature: %d C\n\r", sample_ctemp);
         pc.printf("Temperature: %d K\n\r", sample_ktemp);
@@ -49,8 +96,17 @@ int main()
         pc.printf("Light level %.2f %%\n\r", lightLevel);
         pc.printf("Altitude: %.2f %\n\r", alt);
         pc.printf("Barometric: %.2f %\n\r", bar);
-        pc.printf("Temperature Two: %.2f %\n\r", temp);        
+        pc.printf("Temperature Two: %.2f %\n\r", temp);   
+        pc.printf("\nIMU Temperature = %f C\n\r",25.0 + IMU.temperature/16.0);
+        pc.printf("        X axis    Y axis    Z axis\n\r");
+        pc.printf("gyro:  %9f %9f %9f in deg/s\n\r", IMU.calcGyro(IMU.gx), IMU.calcGyro(IMU.gy), IMU.calcGyro(IMU.gz));
+        pc.printf("accel: %9f %9f %9f in Gs\n\r", IMU.calcAccel(IMU.ax), IMU.calcAccel(IMU.ay), IMU.calcAccel(IMU.az));
+        pc.printf("mag:   %9f %9f %9f in gauss\n\r", IMU.calcMag(IMU.mx), IMU.calcMag(IMU.my), IMU.calcMag(IMU.mz));
+        
+        printAttitude(IMU.calcAccel(IMU.ax), IMU.calcAccel(IMU.ay), IMU.calcAccel(IMU.az), IMU.calcMag(IMU.mx), IMU.calcMag(IMU.my), IMU.calcMag(IMU.mz));    
+        
         pc.printf("\n\r");
-        wait(.2);
+        
+        wait(2);
     }
 }
